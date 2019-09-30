6869aee8ee7204ef6769710ec56bfe2b3729c929
thorlacius <47850061+thorlacius@users.noreply.github.com>
2019-09-30 14:07:47 +0000

    Test URI resolution

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,6 @@
-describe('page', () => {
-  it('works', () => {})
+describe('URI resolver', () => {
+  it('recognizes an absolute URI and normalizes it', () => {
+    cy.visit('https:adversary.io');
+    cy.url().should('eq', 'https://adversary.io/')
+  })
 })
